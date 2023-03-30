136795e7f1ee2b1a3d45e20487bb825854b3f7c1
Arki93 <arakotobes@gmail.com>
2023-03-30 21:31:09 +0200

    Update 30-03 21:30

--- website/urls.py
+++ website/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home_view, name='home'),
-    path('login/', views.login_user, name='login'),
+    path('', views.home, name='home'),
+    # path('login/', views.login_user, name='login'),
     path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
 ]
--- website/views.py
+++ website/views.py
@@ -1,27 +1,52 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignupForm
+from .models import Record
 
-def home_view(request):
-    if request.method == 'POST':
-        user_id = request.POST['email_user']
-        user_passwd = request.POST['passwd_user']
-        user = authenticate(request, username=user_id, password=user_passwd)
-        
+def home(request):
+    records = Record.objects.all()
+    
+    if request.method == "POST":
+        username = request.POST["username"]
+        passwd = request.POST["password"]
+        user = authenticate(request, username=username, password=passwd)
+
         if user is not None:
             login(request, user)
-            messages.success(request, "Login Successful")
-            return redirect('home')
+            messages.success(request, "Login Successful!")
+            return redirect("home")
         else:
             messages.warning(request, "Email or Password is not correct! Try again.")
-            return redirect('home')
+            return redirect("home")
     else:
-        return render(request, 'home.html', {})  
+        return render(request, "home.html", {'records':records})
 
-def login_user(request):
-    pass
+
+# def login_user(request):
+#     pass
+
 
 def logout_user(request):
     logout(request)
     messages.success(request, "You've been logged out!")
-    return redirect('home')
+    return redirect("home")
+
+
+def register_user(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data["username"]
+            passwd = form.cleaned_data["password1"]
+            user = authenticate(username=username, password=passwd)
+            login(request, user)
+            messages.success(request, "You have successfully registered!")
+            return redirect("home")
+    else:
+        form = SignupForm()
+        return render(request, "register.html", {"form": form})
+
+    return render(request, "register.html", {"form": form})
